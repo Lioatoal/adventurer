5eaf53ddb460c1cb896c6143c4384e403278d484
roy <roy20423@hotmail.com>
2019-03-25 11:53:30 +0800

    upgrade

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const fs = require('fs');
+const bodyParser = require('body-parser'); 
 
 const indexRouter = require('./routes/index');
 const systemAPI = require('./routes/systemAPI');
@@ -13,12 +14,29 @@ const app = express();
 /*
  * express module config
 */
-const logFile = fs.createWriteStream('./network.log', {flags: 'a'});
-app.use(logger('dev', {stream: logFile}));
+// const logFile = fs.createWriteStream('./network.log', {flags: 'a'});
+// app.use(logger('dev', {stream: logFile}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Body parser config
+ */
+app.use(bodyParser.json({
+  "type": "application / json",
+  "limit": "50mb", 
+  "extended": true
+}));
+app.use(bodyParser.urlencoded({
+  "limit": "50mb",
+  "extended": false
+}));
+app.use(bodyParser.raw({ 
+  "type": "application/octet-stream", 
+  "limit": "50mb", 
+  "extended": true
+}));
 /*
  * Router Define
 */
